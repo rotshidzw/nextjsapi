d93220504f9cab7658eb3ee8155d64593d192fb4
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2023-03-10 16:39:49 +0200

    removing video file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 
-import Vid from '/src/components/Videos'
+import Vid from '/src/components/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-    return ( <
-        >
-
-        <
-        Vid / >
-
-        <
-        />
-    )
-};
+    return (
+<> 
+<Vid />
+</>
+    )}
