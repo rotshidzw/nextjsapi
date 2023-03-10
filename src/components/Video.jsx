1705a0249accbe09b8eeb41ca210ea719f56255d
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2023-03-10 18:29:04 +0200

    adding  videos again

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,4 +1,4 @@
-import  React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Layout from '../components/Layout';
 
